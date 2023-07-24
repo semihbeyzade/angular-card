72e97b4b310143947581a2459afb4ef5bac32051
semihbeyzade <semihb.orscelik0@gmail.com>
2023-07-24 16:42:27 +0200

    Added Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'card';
+  posts = [
+    {
+      title: "Dolphin",
+      imageUrl: "/assets/dolphin.jpg",
+      username:"Dolphin",
+      content: "The dolphin is a marine mammal belonging to the family Delphinidae. These social and intelligent animals are part of the whale order and can be found in the world's oceans."
+    },
+    {
+      title: "Panda",
+      imageUrl: "/assets/panda.jpg",
+      username:"Panda",
+      content: "Pandas are large mammals with a distinctive black and white coloration. These adorable creatures primarily feed on bamboo leaves and are typically found in the mountainous regions of China."
+    },
+    {
+      title: "Tiger",
+      imageUrl: "/assets/tiger.jpg",
+      username:"Tiger",
+      content: "Tigers are majestic and powerful big cats known for their distinctive orange fur with black stripes. They are skilled hunters and mainly inhabit forests and grasslands in various parts of Asia, displaying a solitary and territorial nature."
+    }
+
+  ]
 }
